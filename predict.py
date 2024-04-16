3e5201d8b6d0a77333ed9a5abf09ea5917017c44
guoX66 <guox66@mail2.sysu.edu.cn>
2024-04-16 23:10:43 +0800

    v1.0.1

--- predict.py
+++ predict.py
@@ -23,16 +23,9 @@ class PredictModel(BaseModel):
         self.model_path = args.model_path
         self.txt_list = []
 
-    def predict_class(self, path, model, data_class):
-        transform = transforms.Compose([
-            transforms.Resize([self.size[0], self.size[1]]),
-            transforms.ToTensor(),
-            transforms.Normalize(self.ms[0], self.ms[1])
-        ])
+    def predict_class(self, image, model, data_class):
         model.eval()
         model = model.to(self.device)
-        image = Image.open(path).convert('RGB')
-        image = transform(image)
         image = torch.reshape(image, (1, 3, self.size[0], self.size[1]))  # 修改待预测图片尺寸，需要与训练时一致
         image = image.to(self.device)
         with torch.no_grad():
@@ -41,33 +34,38 @@ class PredictModel(BaseModel):
         return predictclass
 
     def ensemble_predict(self, model_list, class_dict, img):
+        transform = transforms.Compose([
+            transforms.Resize([self.size[0], self.size[1]]),
+            transforms.ToTensor(),
+            transforms.Normalize(self.ms[0], self.ms[1])
+        ])
+        img = transform(img)
         result_dict = {class_dict[i]: 0 for i in class_dict}
-        file, path = img
         for model in model_list:
-            result = self.predict_class(path, model, class_dict)
+            result = self.predict_class(img, model, class_dict)
             result_dict[result] += 1
         max_key = max(result_dict, key=lambda k: result_dict[k])  # 股价最高的股票名
         if result_dict[max_key] / len(model_list) >= self.min_pr:
             ans = max_key
         else:
-            ans = '未知类别'
+            ans = 'unknown'
         return ans
 
     def imgs_predict(self):
         with open("log/class_id.json", 'r', encoding='UTF-8') as f:
             class_dict = json.load(f)
         class_dict = {int(k): class_dict[k] for k in class_dict.keys()}
-
-        img_list = [[i, os.path.join(args.predict_path, i)] for i in list(os.walk(args.predict_path))[0][-1]
+        img_list = [[i, os.path.join(self.predict_path, i)] for i in list(os.walk(self.predict_path))[0][-1]
                     if i.split('.')[0] != '']  # 获取[图片名,图片路径] 列表
-
         model_list = [[i.split('.')[0], os.path.join(self.model_path, i)] for i in list(os.walk(self.model_path))[0][-1]
                       if
                       i.split('.')[0] != '']  # 获取[模型名,模型路径] 列表
         model_list = [self.make_model(i[0].split('-')[-1], len(class_dict.keys()), i[1]) for i in model_list]
         # 获取[模型,模型路径]列表
         for img in img_list:
-            ans = self.ensemble_predict(model_list, class_dict, img)
+            file, source = img
+            image = Image.open(source).convert('RGB')
+            ans = self.ensemble_predict(model_list, class_dict, image)
             self.txt_list.append([img[0], ans])
             print(f'{img[0]}的预测类别为{ans}')
 
